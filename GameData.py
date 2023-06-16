d2a9278ce2c343a048416cb05a5371ffeda68e44
hackzy <819387214@qq.com>
2023-06-16 13:56:44 +0800

    111

--- GameData.py
+++ GameData.py
@@ -1,10 +1,13 @@
 
 class GameData:
     def __init__(self) -> None:
-        self.角色名
-        self.角色id
-        self.GID
-        self.角色气血
-        self.角色法力
-        self.角色技能
+        self.角色名 = ''
+        self.角色id = 0
+        self.GID = 0
+        self.角色气血 = 0
+        self.角色法力 = 0
+        self.角色技能 = {}
+        self.形象id = 0
+        self.等级 = 0
         
+
--- otherKing.py
+++ otherKing.py
@@ -2,15 +2,32 @@ from bufferWrit import 写封包
 from recBuffer import 读封包
 from setting import *
 class 基础功能:
-    def __init__(self,server) -> None:
-        self.server = server
-    def 中心提醒(self,内容):
+
+    def 中心提示(self,内容):
         写 = 写封包()
         完整包 = 写封包()
         写.写字节集(bytes.fromhex('1fe5'))
         写.写文本型(内容,True,1,True)
-        写.写整数型(0,True)
+        写.写整数型(0)
         完整包.写字节集(组包包头)
-        完整包.写字节集(写.取数据,True,1,True)
+        完整包.写字节集(写.取数据(),True,1,True)
+
         return 完整包.取数据()
 
+    def 左下角提示(self,内容):
+        写 = 写封包()
+        完整包 = 写封包()
+        写.写字节集(bytes.fromhex('2fff'))
+        写.写整数型(0,True)
+        写.写整数型(0,True)
+        写.写文本型(内容,True)
+        写.写字节集(bytes.fromhex('00 00 00\
+                     00 00 00 12 E6 9B B4 E9\
+                     91 84 E8 BC 9D E7 85 8C \
+                    E4 B8 80 E7 B7 9A 00 01'\
+                             .replace(" ","")))
+        完整包.写字节集(组包包头)
+        完整包.写字节集(写.取数据(),True,1,True)
+        return 完整包.取数据()
+
+    def 
