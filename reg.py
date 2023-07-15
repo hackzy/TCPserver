569fc18dfbd207dce3b60fda12dc54fd18013080
hackzy <819387214@qq.com>
2023-07-15 15:23:43 +0800

    修复注册bug

--- reg.py
+++ reg.py
@@ -31,7 +31,7 @@ class Reg:
             cur.execute(insert)
             self.mysql.commit()
             self.mysql.close()
-            return '注册成功！'
+            return '注册成功！账号:%s 密码:%s'%(账号密码[0],账号密码[1])
         else:
             self.mysql = pymysql.connect(host='127.0.0.1',password='Chen1021',user='root',charset='utf8',database='adb')
             self.mysql.close()
