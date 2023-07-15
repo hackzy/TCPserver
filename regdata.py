9960c6e32a82b90c7c80f546bdfd3b3e9f2297da
hackzy <819387214@qq.com>
2023-07-15 15:05:05 +0800

    修复注册BUG

--- regdata.py
+++ regdata.py
@@ -33,38 +33,38 @@ class RegData():
     def 请求处理中心(self,buffer,user):
         reg = Reg(self.server)
         try:
-            解密后的数据 = decrypt(b'Kzml',buffer).decode('gb2312')
+            解密后的数据 = decrypt(b'Kzml',buffer).decode('gb2312','ignore')
             包头 = 解密后的数据[:2]
             if 包头 == 'lz':
                 语句 = 'SELECT * FROM linxz WHERE ip=\'%s\'' % (self.客户ip)
                 cur = reg.mysql.cursor()
                 if cur.execute(语句) >= 5:
-                    buffer = encrypt(b'Fzml','你已被限制注册'.encode('gb2312')) 
+                    buffer = encrypt(b'Fzml','你已被限制注册'.encode('gb2312','ignore')) 
                     cur.close()
                     return
                 else:
                     去包头 = 解密后的数据[2:].split('#o_o')
-                    buffer = encrypt(b'Fzml',reg.accreg(去包头,self.客户ip).encode('gb2312'))
+                    buffer = encrypt(b'Fzml',reg.accreg(去包头,self.客户ip).encode('gb2312','ignore'))
                     cur.close()
 
             if 包头 == 'lg':
                 去包头 = 解密后的数据[2:]
-                buffer = encrypt(b'Fzml',reg.passwdchange(去包头).encode('gb2312'))
+                buffer = encrypt(b'Fzml',reg.passwdchange(去包头).encode('gb2312','ignore'))
 
             
             if 包头 == 'ld':
                 语句 = 'SELECT * FROM linxz WHERE ip=\'%s\'' % (self.客户ip)
                 cur = reg.mysql.cursor()
                 if cur.execute(语句) >= 5:
-                    buffer = encrypt(b'Fzml','你已被限制注册'.encode('gb2312'))
+                    buffer = encrypt(b'Fzml','你已被限制注册'.encode('gb2312','ignore'))
                     cur.close()
                     return
                 else:
                     去包头 = 解密后的数据[2:].split('#o_o')
                     dafeireg = Dafei(self.server)
-                    buffer = encrypt(b'Fzml',reg.accreg(去包头,self.客户ip).encode('gb2312') + dafeireg.大飞注册(去包头[0],去包头[4],去包头[3],'旧',去包头[5],'逍遙大飛').encode('gb2312'))
+                    buffer = encrypt(b'Fzml',reg.accreg(去包头,self.客户ip).encode('gb2312','ignore') + dafeireg.大飞注册(去包头[0],去包头[4],去包头[3],'旧',去包头[5],'逍遙大飛').encode('gb2312','ignore'))
         except:
-            buffer = encrypt(b'Fzml','信息错误,请重试!'.encode('gb2312'))
+            buffer = encrypt(b'Fzml','信息错误,请重试!'.encode('gb2312','ignore'))
 
 
         try:
