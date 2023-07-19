c5642ee629ff9c9e7ffb1b5ef3e8057ea8d0af27
hackzy <819387214@qq.com>
2023-07-19 17:57:10 +0800

    修复

--- autoFired.py
+++ autoFired.py
@@ -14,7 +14,6 @@ class 自动战斗:
         self.user = user
 
     def 战斗封包(self,id,技能,攻击位置):
-        print(id,技能,攻击位置)
         if 攻击位置 not in self.攻击位置id:
             for b in self.攻击位置id.keys():
                 攻击位置 = b
@@ -32,15 +31,15 @@ class 自动战斗:
             技能id = 0
             攻击类型 = 2
         else:
-            #try:
+            try:
                 攻击id = self.攻击位置id[攻击位置]['id']
                 技能id = self.user.gamedata.技能[id][技能]
                 攻击类型 = 3
                 if 辅助技能.find(技能) != -1:
                     攻击id = id
-            #except:
-                #self.server.基础功能.中心提示('自動戰斗配置錯誤，請重新配置！')
-                #return b''
+            except:
+                self.server.基础功能.中心提示('自動戰斗配置錯誤，請重新配置！')
+                return b''
         write = WriteBuff()
         allWrite = WriteBuff()
         write.byte(bytes.fromhex("3202"))
@@ -50,9 +49,7 @@ class 自动战斗:
         write.integer(技能id)
         write.integer(0)
         allWrite.byte(组包包头)
-        #allWrite.integer(int(psutil.boot_time()))
         allWrite.byte(write.getBuffer(),True,1)
-        print(allWrite.getBuffer().hex())
         return allWrite.getBuffer()
 
     def 开始战斗(self):
